31aed84f77c2614733dab786d125eb9555ae4d26
Himanshu Gusain <himu5493@gmail.com>
2023-09-09 17:28:05 +0000

    Non-generic sums

---
+++ generics/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+// SumsInts adds togehter the values of m.
+func SumsInts(m map[string]int64) int64 {
+	var s int64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumFloats add together the values of m.
+func SumFloats(m map[string]float64) float64 {
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func main() {
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+
+	fmt.Printf("Non Generic Sums: %v, %v", SumsInts(ints), SumFloats(floats))
+}
